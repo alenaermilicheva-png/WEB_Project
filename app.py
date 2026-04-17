a29ea4005fb8c7403f57b3749ec6b29312f76bde
alena <alenaermilicheva@gmail.com>
2026-04-17 17:46:58 +0300

    начало2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from data.login import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -7,6 +8,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect("/")
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
