0c983e055718ff57d1bfcdc23ed9025c4bf4c225
alena <alenaermilicheva@gmail.com>
2026-04-25 12:48:14 +0300

    app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, redirect
 from data.login import LoginForm
+from data import db_session
+from data.models import Event
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -7,10 +9,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    session = db_session.create_session()
+    events = session.query(Event).all()
+    session.close()
+    return render_template('index.html', events=events)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', ' POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -19,4 +24,5 @@ def login():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/dobro.db")
     app.run(port=8080, host='127.0.0.1')
